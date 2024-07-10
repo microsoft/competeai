332e7edbedcfa2f1abaef81f0b02e0adf9fe4597
Jindong Wang <jindongwang@outlook.com>
2024-07-10 16:07:14 +0800

    Update base.py

--- database/restaurant_sys/restaurant_sys/settings/base.py
+++ database/restaurant_sys/restaurant_sys/settings/base.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-^kummsxftm9y0k$-m3!$^ugu^i0ui-yf(ttw51%7*hl5gn&4oc"
+SECRET_KEY = "key"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
